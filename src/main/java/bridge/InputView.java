60b7dad3b9ba293dbb459fb0c4cb1612ac835025
ffolabear <ffolabear@gmail.com>
2022-11-17 03:01:50 +0900

    chore(InputView): 편의를 위해 검증 메서드에 대한 정보를 주석으로 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -10,59 +10,63 @@ public class InputView {
 
     /**
      * 다리의 길이를 입력받는다.
+     * 검증메서드 : isLengthDigit -> convertInput -> isLengthValidRange
      */
     public String readBridgeSize() {
-        String lengthInput = Console.readLine();
-        return lengthInput;
+        return Console.readLine();
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
+     * 검증메서드 : isValidGameCommand
      */
     public String readMoving() {
-        return null;
+        return Console.readLine();
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+     * 검증메서드 : isValidReplayCommand
      */
     public String readGameCommand() {
-        return null;
+        return Console.readLine();
     }
 
 
+    /*
+        추가 구현 코드
+     */
 
-    private int convertInput(String input) {
+    public int convertInput(String input) {
         return Integer.parseInt(input);
     }
 
     /*
         입력값 검증부
      */
-    private void isLengthDigit(String lengthInput) {
-        if (!lengthInput.matches("[0-9]+")){
+    public void isLengthDigit(String lengthInput) {
+        if (!lengthInput.matches("[0-9]+")) {
             throw new IllegalArgumentException(Errors.ERROR_NON_DIGIT.getError());
         }
     }
 
-    private void isLengthValidRange(int convertedInput) {
-        if (convertedInput < 3  || 20 < convertedInput){
+    public void isLengthValidRange(int convertedInput) {
+        if (convertedInput < 3 || 20 < convertedInput) {
             throw new IllegalArgumentException(Errors.ERROR_INVALID_BRIDGE_LENGTH.getError());
         }
     }
 
-    private void isValidGameCommand(String gameCommandInput) {
+    public void isValidGameCommand(String gameCommandInput) {
         if (!gameCommandInput.equals("U") && !gameCommandInput.equals("D")) {
             throw new IllegalArgumentException(Errors.ERROR_INVALID_GAME_COMMAND.getError());
         }
     }
 
-    private void isValidReplayCommand(String gameCommandInput) {
+    public void isValidReplayCommand(String gameCommandInput) {
         if (!gameCommandInput.equals("R") && !gameCommandInput.equals("Q")) {
             throw new IllegalArgumentException(Errors.ERROR_INVALID_REPLAY_COMMAND.getError());
         }
     }
-
 
 
 }
