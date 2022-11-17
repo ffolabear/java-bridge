c29b68e4e86caf1aad1a895f7302727ffca39d41
ffolabear <ffolabear@gmail.com>
2022-11-18 04:21:11 +0900

    feat(Round): 게임당 존재하는 라운드를 생성하는 클래스 생성

---
+++ src/main/java/bridge/service/Round.java
@@ -0,0 +1,42 @@
+package bridge.service;
+
+import bridge.util.InputView;
+import bridge.validation.InputValidation;
+import bridge.view.OutputView;
+
+import java.util.List;
+
+public class Round {
+
+    List<String> answerBridge;
+    List<String> userBridge;
+    InputView inputView;
+    OutputView outputView;
+    InputValidation validator;
+    String command;
+    int currentRound;
+
+    public Round(List<String> answerBridge, List<String> userBridge, int currentRound) {
+        this.answerBridge = answerBridge;
+        this.userBridge = userBridge;
+        this.currentRound = currentRound;
+        inputView = new InputView();
+        outputView = new OutputView();
+        validator = new InputValidation();
+    }
+
+    public void startRound() {
+        String input = inputView.readGameCommand();
+        validator.isValidGameCommand(command);
+        command = input;
+    }
+
+    public boolean judge() {
+        return answerBridge.get(currentRound).equals(command);
+    }
+
+    public void printRoundResult() {
+
+    }
+
+}
