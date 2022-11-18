7d1434400b6a50df91779832e773d6dede1fb5a6
ffolabear <ffolabear@gmail.com>
2022-11-18 19:46:34 +0900

    feat(Application): 어플리케이션의 실행부분 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,16 @@
 package bridge;
 
+import bridge.service.BridgeGame;
+import bridge.service.GameController;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            BridgeGame bridgeGame = new BridgeGame();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
