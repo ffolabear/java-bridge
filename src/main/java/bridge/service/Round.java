06f9fa46375764a8ea4c12db6740c52251c3d9a7
ffolabear <ffolabear@gmail.com>
2022-11-19 23:47:42 +0900

    feat(Round): 라운드 진행에 필요한 기능 구현

--- src/main/java/bridge/service/Round.java
+++ src/main/java/bridge/service/Round.java
@@ -3,40 +3,87 @@ package bridge.service;
 import bridge.util.InputView;
 import bridge.validation.InputValidation;
 import bridge.util.OutputView;
+import bridge.view.Messages;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Round {
 
-    List<String> answerBridge;
-    List<String> userBridge;
+    final List<String> answerBridge;
+    final List<String> userBridge;
+    List<List<String>> resultBridge;
     InputView inputView;
     OutputView outputView;
     InputValidation validator;
-    String command;
-    int currentRound;
 
-    public Round(List<String> answerBridge, List<String> userBridge, int currentRound) {
+    public Round(List<String> answerBridge, List<String> userBridge) {
         this.answerBridge = answerBridge;
         this.userBridge = userBridge;
-        this.currentRound = currentRound;
         inputView = new InputView();
         outputView = new OutputView();
         validator = new InputValidation();
+        resultBridge = new ArrayList<>();
     }
 
-    public void startRound() {
-        String input = inputView.readGameCommand();
-        validator.isValidGameCommand(command);
-        command = input;
+    public void start() {
+        makeRoundResult();
+        printRoundResult();
     }
 
-    public boolean judge() {
-        return answerBridge.get(currentRound).equals(command);
+    public String inputCommand() {
+        String command = inputView.readMoving();
+        validator.validateGameCommand(command);
+        return command;
+    }
+
+    public void makeRoundResult() {
+        for (int index = 0; index < userBridge.size(); index++) {
+            List<String> moveResult = makeMoveResult(index);
+            resultBridge.add(moveResult);
+            if (moveResult.get(1).equals(GameAttribute.UNCROSSABLE.getAttribute())) {
+                break;
+            }
+        }
+    }
+
+    private List<String> makeMoveResult(int index) {
+        List<String> moveResult = new ArrayList<>();
+        moveResult.add(userBridge.get(index));
+        moveResult.add(judge(index));
+        return moveResult;
+    }
+
+    public String judge(int index) {
+        if (userBridge.get(index).equals(answerBridge.get(index))) {
+            return GameAttribute.CROSSABLE.getAttribute();
+        }
+        return GameAttribute.UNCROSSABLE.getAttribute();
+    }
+
+    public boolean continueRound() {
+        int lastIndex = userBridge.size() - 1;
+        if (!userBridge.isEmpty() && !userBridge.get(lastIndex).equals(answerBridge.get(lastIndex))) {
+            return false;
+        }
+        return true;
     }
 
     public void printRoundResult() {
+        outputView.printMap(resultBridge);
+    }
 
+    public void printFinalResult() {
+        outputView.printResult(resultBridge);
+    }
+
+    public String retryRound() {
+        outputView.printSystemMessage(Messages.GAME_RETRY_MESSAGE);
+        String command = inputView.readGameCommand();
+        if (command.equals(GameAttribute.QUIT.getAttribute())) {
+
+        }
+        return inputView.readGameCommand();
     }
 
 }
