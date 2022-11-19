847af409ba3a17acc19b2cdd7ac44c9718bed861
ffolabear <ffolabear@gmail.com>
2022-11-19 23:48:07 +0900

    chore(Messages): 상수 이름 직관성 있게 변경

--- src/main/java/bridge/view/Messages.java
+++ src/main/java/bridge/view/Messages.java
@@ -5,7 +5,7 @@ public enum Messages {
     GAME_START_MESSAGE("다리 건너기 게임을 시작합니다."),
     INPUT_BRIDGE_LENGTH_MESSAGE("\n다리의 길이를 입력해주세요."),
     INPUT_GAME_COMMAND_MESSAGE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
-    GAME_REPLAY_MESSAGE("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+    GAME_RETRY_MESSAGE("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     GAME_RESULT_TITLE("최종 게임 결과"),
     GAME_RESULT_MESSAGE("\n게임 성공 여부: "),
     GAME_TRIAL_MESSAGE("총 시도한 횟수: ");
