8a9f800cf8d876dc1d3c79d7b135306f23858c89
ffolabear <ffolabear@gmail.com>
2022-11-20 22:41:45 +0900

    chore(BridgeMaker): 입력값의 검증 부분 제거

--- src/main/java/bridge/service/BridgeMaker.java
+++ src/main/java/bridge/service/BridgeMaker.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- *
+ * <p>
  * 필드(인스턴스 변수)를 변경할 수 없다.
  * 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 없다.
  */
@@ -23,21 +23,19 @@ public class BridgeMaker {
 
     public int inputBridgeLength() {
         InputView inputView = new InputView();
-        InputValidation validator = new InputValidation();
-
         String length = inputView.readBridgeSize();
-        validator.validateBridgeLength(length);
-        return validator.convertInput(length);
+        inputView.validateBridgeLength();
+
+        return inputView.convertInput(length);
     }
 
     /**
      * @param size 다리의 길이
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     *
+     * <p>
      * public List<String> makeBridge(int size) {
-     *         return null;
+     * return null;
      * }
-     *
      */
     public List<String> makeBridge(int size) {
         List<String> bridge = new ArrayList<>();
