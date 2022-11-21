ae194c8b550dd2b9e1a6a92b793f240690fb376c
ffolabear <ffolabear@gmail.com>
2022-11-22 05:10:04 +0900

    chore(InputView): 불필요한 부분 제거 및 접근 제한자 변경

--- src/main/java/bridge/util/InputView.java
+++ src/main/java/bridge/util/InputView.java
@@ -1,25 +1,21 @@
 package bridge.util;
 
-import bridge.validation.InputValidation;
+import bridge.validation.Validation;
 import bridge.view.Messages;
 import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
- * <p>
- * 패키지는 변경할 수 있다.
- * 메서드의 시그니처(인자, 이름)와 반환 타입은 변경할 수 있다.
- * 사용자 값 입력을 위해 필요한 메서드를 추가할 수 있다.
  */
 public class InputView {
 
-    InputValidation inputValidation;
-    String bridgeLength;
-    String command;
-    String retry;
+    Validation inputValidation;
+    private String bridgeLength;
+    private String command;
+    private String retry;
 
     public InputView() {
-        inputValidation = new InputValidation();
+        inputValidation = new Validation();
     }
 
     public void validateBridgeLength() {
@@ -36,7 +32,6 @@ public class InputView {
 
     /**
      * 다리의 길이를 입력받는다.
-     * 검증메서드 : isLengthDigit -> convertInput -> isLengthValidRange
      */
     public String readBridgeSize() {
         OutputView.printSystemMessage(Messages.INPUT_BRIDGE_LENGTH_MESSAGE);
@@ -46,7 +41,6 @@ public class InputView {
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
-     * 검증메서드 : isValidGameCommand
      */
     public String readMoving() {
         OutputView.printSystemMessage(Messages.INPUT_GAME_COMMAND_MESSAGE);
@@ -56,7 +50,6 @@ public class InputView {
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     * 검증메서드 : isValidReplayCommand
      */
     public String readGameCommand() {
         OutputView.printSystemMessage(Messages.GAME_RETRY_MESSAGE);
@@ -64,10 +57,6 @@ public class InputView {
         return retry;
     }
 
-
-    /*
-        추가 구현 코드
-     */
     public int convertInput(String input) {
         return Integer.parseInt(input);
     }
