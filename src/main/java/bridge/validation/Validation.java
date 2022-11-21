30bc6975189e4d44f02ed05c67a41ba481ab9bfa
ffolabear <ffolabear@gmail.com>
2022-11-22 05:11:50 +0900

    refactor(InputValidation): 클래스 이름 변경 및 불필요한 부분 제거

--- src/main/java/bridge/validation/Validation.java
+++ src/main/java/bridge/validation/Validation.java
@@ -2,46 +2,42 @@ package bridge.validation;
 
 import bridge.view.Errors;
 
-public class InputValidation {
+public class Validation {
 
     private final String errorHead = Errors.ERROR_HEAD.getError();
 
     public void validateBridgeLength(String input) {
-        isLengthDigit(input);
-        isLengthValidRange(Integer.parseInt(input));
+        validateLengthDigit(input);
+        validateLengthRange(Integer.parseInt(input));
     }
 
-    public void validateGameCommand(String input) {
-        isValidGameCommand(input);
-    }
-
-    public void validateRetryCommand(String input) {
-        isValidReplayCommand(input);
-    }
-
-    public void isLengthDigit(String input) {
+    private void validateLengthDigit(String input) {
         if (!input.matches("[0-9]+")) {
             throw new IllegalArgumentException(errorHead + Errors.ERROR_NON_DIGIT.getError());
         }
     }
 
-    public void isLengthValidRange(int convertedInput) {
+    private void validateLengthRange(int convertedInput) {
         if (convertedInput < 3 || 20 < convertedInput) {
             throw new IllegalArgumentException(errorHead + Errors.ERROR_INVALID_BRIDGE_LENGTH.getError());
         }
     }
 
-    public void isValidGameCommand(String gameCommandInput) {
+    public void validateGameCommand(String gameCommandInput) {
         if (!gameCommandInput.equals("U") && !gameCommandInput.equals("D")) {
             throw new IllegalArgumentException(errorHead + Errors.ERROR_INVALID_GAME_COMMAND.getError());
         }
     }
 
-    public void isValidReplayCommand(String gameCommandInput) {
+    public void validateRetryCommand(String gameCommandInput) {
         if (!gameCommandInput.equals("R") && !gameCommandInput.equals("Q")) {
             throw new IllegalArgumentException(errorHead + Errors.ERROR_INVALID_REPLAY_COMMAND.getError());
         }
     }
 
-
+    public void validateTrialNumber(long trial) {
+        if (trial > Integer.MAX_VALUE) {
+            throw new IllegalArgumentException(errorHead + Errors.ERROR_EXCESS_MAXIMUM_TRIAL.getError());
+        }
+    }
 }
