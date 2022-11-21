472ed99dbcbcc8c4ba05f57566e548c0690f6edf
ffolabear <ffolabear@gmail.com>
2022-11-22 05:51:27 +0900

    feat(GameCommand): 게임 진행중 명령어들을 상수로 갖는 enum 생성

---
+++ src/main/java/bridge/service/GameCommand.java
@@ -0,0 +1,19 @@
+package bridge.service;
+
+public enum GameCommand {
+
+    UP("U"),
+    DOWN("D"),
+    RETRY("R"),
+    QUIT("Q");
+
+    private final String command;
+
+    GameCommand(String command) {
+        this.command = command;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+}
