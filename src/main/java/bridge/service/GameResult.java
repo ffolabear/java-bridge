af632fabfd56e6bfe34539dd3bad1247e7791565
ffolabear <ffolabear@gmail.com>
2022-11-22 05:52:06 +0900

    refactor(GameAttribute): enum 이름 변경 및 상수 이동

--- src/main/java/bridge/service/GameResult.java
+++ src/main/java/bridge/service/GameResult.java
@@ -1,19 +1,15 @@
 package bridge.service;
 
-public enum GameAttribute {
+public enum GameResult {
 
-    UP("U"),
-    DOWN("D"),
     CROSSABLE("O"),
     UNCROSSABLE("X"),
-    RETRY("R"),
-    QUIT("Q"),
     SUCCESS("성공"),
     FAIL("실패");
 
     private final String attribute;
 
-    GameAttribute(String attribute) {
+    GameResult(String attribute) {
         this.attribute = attribute;
     }
 
