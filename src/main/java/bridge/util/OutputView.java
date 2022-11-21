5f0c1245a6c5784baedfaed028355cf5cd7a30f7
ffolabear <ffolabear@gmail.com>
2022-11-21 17:08:43 +0900

    chore(OutputView): 메소드 선언 부분 수정

--- src/main/java/bridge/util/OutputView.java
+++ src/main/java/bridge/util/OutputView.java
@@ -13,16 +13,12 @@ import java.util.List;
  */
 public class OutputView {
 
-    public OutputView() {
-
-    }
-
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap(List<List<String>> resultBridge) {
+    public static void printMap(List<List<String>> resultBridge) {
         BridgeFormat bridgeFormat = new BridgeFormat(resultBridge);
         System.out.println(bridgeFormat.formattedBridge());
     }
@@ -33,21 +29,22 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(List<List<String>> resultBridge) {
-        BridgeFormat bridgeFormat = new BridgeFormat(resultBridge);
+    public static void printResult(List<List<String>> resultBridge, String gameResult, int trial) {
         printSystemMessage(Messages.GAME_RESULT_TITLE);
-        System.out.println(bridgeFormat.formattedBridge());
+        printMap(resultBridge);
+        printGameResultMessage(Messages.GAME_RESULT_MESSAGE, gameResult);
+        printGameTrialMessage(Messages.GAME_TRIAL_MESSAGE, trial);
     }
 
-    public void printSystemMessage(Messages messages) {
+    public static void printSystemMessage(Messages messages) {
         System.out.println(messages.getMessage());
     }
 
-    public void printGameResultMessage(Messages messages, String gameResult) {
+    public static void printGameResultMessage(Messages messages, String gameResult) {
         System.out.printf((messages.getMessage()) + "%n", gameResult);
     }
 
-    public void printGameTrialMessage(Messages messages, int trial) {
-        System.out.printf((messages.getMessage()) + "%n", String.valueOf(trial));
+    public static void printGameTrialMessage(Messages messages, int trial) {
+        System.out.printf((messages.getMessage()) + "%n", trial);
     }
 }
