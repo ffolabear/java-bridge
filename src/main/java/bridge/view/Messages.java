6ba5c47225848a1da7772612c93214d97eb67cc9
ffolabear <ffolabear@gmail.com>
2022-11-23 04:00:45 +0900

    fix(Messages): 게임 재시도 취소시 공백 한줄 출력되던 오류 수정

--- src/main/java/bridge/view/Messages.java
+++ src/main/java/bridge/view/Messages.java
@@ -6,7 +6,7 @@ public enum Messages {
     INPUT_BRIDGE_LENGTH_MESSAGE("\n다리의 길이를 입력해주세요."),
     INPUT_GAME_COMMAND_MESSAGE("\n이동할 칸을 선택해주세요. (위: U, 아래: D)"),
     GAME_RETRY_MESSAGE("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
-    GAME_RESULT_TITLE("\n최종 게임 결과"),
+    GAME_RESULT_TITLE("최종 게임 결과"),
     GAME_RESULT_MESSAGE("\n게임 성공 여부: %s"),
     GAME_TRIAL_MESSAGE("총 시도한 횟수: %s");
 
