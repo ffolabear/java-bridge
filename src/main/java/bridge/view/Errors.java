7dca81c3afe3746f56dc45a66296ddfbaa44c4c0
ffolabear <ffolabear@gmail.com>
2022-11-22 19:17:00 +0900

    feat(Errors): 새로운 에러 추가

--- src/main/java/bridge/view/Errors.java
+++ src/main/java/bridge/view/Errors.java
@@ -4,6 +4,8 @@ public enum Errors {
 
     ERROR_HEAD("[ERROR] "),
     ERROR_NON_DIGIT("다리 길이는 반드시 숫자여야 합니다."),
+    ERROR_EMPTY_INPUT("입력값을 입력하지 않았습니다."),
+    ERROR_EXCESS_INTEGER("다리 길이는 3부터 20 사이의 숫자여야 합니다."),
     ERROR_INVALID_BRIDGE_LENGTH("다리 길이는 3부터 20 사이의 숫자여야 합니다."),
     ERROR_INVALID_GAME_COMMAND("유효하지 않은 명령어입니다."),
     ERROR_INVALID_REPLAY_COMMAND("유효하지 않은 명령어입니다."),
