e78a943d87433fb9af993d4c946558a31245103e
ffolabear <ffolabear@gmail.com>
2022-11-22 21:53:47 +0900

    refactor(BridgeGame): 주석 제거 및 들여쓰기 단계 수정

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -4,12 +4,10 @@ import bridge.BridgeRandomNumberGenerator;
 
 import java.util.List;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스 -> 컨트롤러
- */
 public class BridgeGame {
 
     private long trial = 1;
+    private boolean continueRound = true;
     private Round round;
     boolean gameResult;
     private List<String> answerBridge;
@@ -36,30 +34,30 @@ public class BridgeGame {
 
     public void playRound() {
         round = new Round(answerBridge);
-        while (round.compareBridgeSize()) {
+        continueRound = round.compareBridgeSize();
+        while (continueRound) {
             move();
-            if (round.continueRound()) {
-                retry();
-                break;
-            }
+            escapeRound();
         }
     }
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void move() {
-        round.userBridge.add(round.inputCommand());
-        round.printRoundResult();
+    public void escapeRound() {
+        if (round.continueRound()) {
+            retry();
+            continueRound = false;
+        }
+        if (round.isCorrectRound()) {
+            continueRound = false;
+        }
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    public void move() {
+        round.userBridge.add(round.inputCommand());
+        if (continueRound) {
+            round.printRoundResult();
+        }
+    }
+
     public void retry() {
         gameResult = false;
         if (round.retryRound()) {
