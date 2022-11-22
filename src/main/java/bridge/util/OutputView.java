5eb324ba90b6ccdae5ff1e89c2c136ac78f41309
ffolabear <ffolabear@gmail.com>
2022-11-22 21:51:01 +0900

    chore(OutputView): 주석 제거

--- src/main/java/bridge/util/OutputView.java
+++ src/main/java/bridge/util/OutputView.java
@@ -6,27 +6,13 @@ import bridge.view.Messages;
 
 import java.util.List;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public static void printMap(List<List<String>> resultBridge) {
         BridgeFormat bridgeFormat = new BridgeFormat(resultBridge);
         System.out.println(bridgeFormat.formattedBridge());
     }
 
-
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public static void printResult(List<List<String>> resultBridge, String gameResult, long trial) {
         Validation validation = new Validation();
         validation.validateTrialNumber(trial);
