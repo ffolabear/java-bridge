a3a83cc13f7051083e198b9c517b37f01c64ccef
ffolabear <ffolabear@gmail.com>
2022-11-23 03:51:25 +0900

    fix(Application): 예외 발생시 어플리케이션을 종료하지 않도록 수정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -7,12 +7,8 @@ import bridge.view.Messages;
 public class Application {
 
     public static void main(String[] args) {
-        try {
-            OutputView.printSystemMessage(Messages.GAME_START_MESSAGE);
-            BridgeGame bridgeGame = new BridgeGame();
-            bridgeGame.startGame();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        OutputView.printSystemMessage(Messages.GAME_START_MESSAGE);
+        BridgeGame bridgeGame = new BridgeGame();
+        bridgeGame.startGame();
     }
 }
